d3cbce7a0d61a38562ee76afd77f644a37dc0feb
Manuel Espinosa Torres <maneletorres@hotmail.com>
2021-06-30 22:31:27 +0200

    Simple testing implemented.

---
+++ app/src/test/java/com/example/googlerepositories/ui/FakeFactory.kt
@@ -0,0 +1,36 @@
+package com.example.googlerepositories.ui
+
+import com.example.googlerepositories.api.repository.GitHubRepository
+import com.example.googlerepositories.data.Owner
+import com.example.googlerepositories.data.Repository
+import com.example.googlerepositories.util.extensions.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlin.random.Random
+
+val fakeRepositories = createFakeRepository()
+
+fun createFakeRepository(): List<Repository> {
+    val repositoryList: ArrayList<Repository> = arrayListOf()
+    for (i in 1..10) {
+        val fork = Random.nextBoolean()
+        val repo = Repository(
+            1,
+            "Name $i",
+            "Full name $i",
+            Owner("Login $i", i.toLong(), "Avatar URL $i"),
+            "Description $i",
+            "HTML URL $i",
+            fork
+        )
+        repositoryList.add(repo)
+    }
+    return repositoryList
+}
+
+class FakeLocalGitHubRepository : GitHubRepository {
+    private val repositories = mutableListOf<Repository>()
+
+    override fun getRepositories(): Flow<Resource<List<Repository>>> =
+        flowOf(Resource.Success(repositories))
+}
